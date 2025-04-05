bd3bf3dca3efe1106ed8a7ff135bf0c6017d4b55
Nikita Bohatyrov <nikita.bohatyrov@airlab.ai>
2025-04-05 19:40:32 +0300

    Initial Commit

---
+++ BalanceAnalyticsApp/BalanceAnalyticsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceAnalyticsAppApp.swift
+//  BalanceAnalyticsApp
+//
+//  Created by никита богатырев on 05.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct BalanceAnalyticsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
